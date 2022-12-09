1307ae9433fa163cd97e940e1e7aade9ff933d3e
Ashish <114793959+Ashish9608@users.noreply.github.com>
2022-12-09 15:04:05 +0530

    Add files via upload

---
+++ class63 for-loop CypherSchools.py
@@ -0,0 +1,12 @@
+# sum from 1 to 10
+# 1+2 +3...
+# total = 0
+# for i in range(1,11): total += i
+# print(total)
+
+
+n = input("enter the number")
+total = 0
+for i in range(1,n+1): 
+    total += i
+print(total)
